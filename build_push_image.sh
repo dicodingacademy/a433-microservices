6e3691288c052d367f6161e2c6787f756aed94d6
bagus-k <baagus.kurniawan@gmail.com>
2023-02-22 21:55:46 +0700

    add build_push_image.sh

---
+++ build_push_image.sh
@@ -0,0 +1,17 @@
+# membuat Docker image
+docker build -t item-app:v1 .
+
+#melihat daftar image
+docker images
+
+#Mengubah nama image
+docker tag item-app:v1 ghcr.io/bagus-k/item-app:v1
+
+#save access token Github
+$GITHUB_ACCESS_TOKEN = "Your Token"
+
+#Login ke GitHub Packages
+echo $GITHUB_ACCESS_TOKEN | docker login ghcr.io -u bagusk --password-stdin 
+
+#Mengunggah image Github Packages
+docker push ghcr.io/bagus-k/item-app:v1
